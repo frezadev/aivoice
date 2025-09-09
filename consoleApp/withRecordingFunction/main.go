155eb889261ec53bf2294d821422693a20abfe0b
frezadev <freza.dev@gmail.com>
2025-09-09 08:58:27 +0700

    add withRecordingFunction

---
+++ consoleApp/withRecordingFunction/main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	openairt "github.com/WqyJh/go-openai-realtime"
+	"github.com/WqyJh/go-openai-realtime/examples/voice/pcm"
+	"github.com/WqyJh/go-openai-realtime/examples/voice/recorder"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/speaker"
+	"github.com/gordonklaus/portaudio"
+	"github.com/joho/godotenv"
+	"github.com/sashabaranov/go-openai"
+)
+
+func getEnv(key string) string {
+	envFile, err := godotenv.Read("../../config.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return envFile[key]
+}
+
+// todo: chatgpt like, no need to start and stop command
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	openaiApiKey := getEnv("OPENAI_API_KEY")
+	client := openairt.NewClient(openaiApiKey)
+	conn, err := client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// Teletype response
+	responseDeltaHandler := func(ctx context.Context, event openairt.ServerEvent) {
+		switch event.ServerEventType() {
+		case openairt.ServerEventTypeResponseAudioTranscriptDelta:
+			// ignore
+		case openairt.ServerEventTypeResponseAudioTranscriptDone:
+			fmt.Printf("[response] %s\n", event.(openairt.ResponseAudioTranscriptDoneEvent).Transcript)
+		}
+	}
+
+	// Full response
+	responseHandler := func(ctx context.Context, event openairt.ServerEvent) {
+		switch event.ServerEventType() {
+		case openairt.ServerEventTypeConversationItemInputAudioTranscriptionCompleted:
+			question := event.(openairt.ConversationItemInputAudioTranscriptionCompletedEvent).Transcript
+			fmt.Printf("\n[question] %s\n", question)
+		case openairt.ServerEventTypeResponseDone:
+			fmt.Print("\n> ")
+		}
+	}
+
+	// Log handler
+	logHandler := func(ctx context.Context, event openairt.ServerEvent) {
+		// fmt.Printf("[%s]\n", event.ServerEventType())
+
+		switch event.ServerEventType() {
+		case openairt.ServerEventTypeError,
+			openairt.ServerEventTypeSessionUpdated:
+			// data, err := json.Marshal(event)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// fmt.Printf("[%s] %s\n", event.ServerEventType(), string(data))
+		}
+	}
+
+	// Init speaker
+	speaker.Init(24000, 24000/10)
+	defer speaker.Close()
+
+	streamers := make(chan *pcm.PCMStream, 100)
+	datas := make(chan []byte, 100)
+	beep.NewBuffer(beep.Format{
+		SampleRate:  beep.SampleRate(24000),
+		NumChannels: 1,
+		Precision:   2, // 16-bit PCM
+	})
+
+	// Audio response
+	audioResponseHandler := func(ctx context.Context, event openairt.ServerEvent) {
+		switch event.ServerEventType() {
+		case openairt.ServerEventTypeResponseAudioDelta:
+			msg := event.(openairt.ResponseAudioDeltaEvent)
+			// log.Printf("audioResponseHandler: %v", delta)
+			data, err := base64.StdEncoding.DecodeString(msg.Delta)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// os.WriteFile(fmt.Sprintf("%s.pcm", msg.EventID), data, 0o644)
+			// streamer.Append(data)
+			datas <- data
+		case openairt.ServerEventTypeResponseAudioDone:
+			fulldata := []byte{}
+			close(datas)
+			for data := range datas {
+				fulldata = append(fulldata, data...)
+			}
+			// os.WriteFile(fmt.Sprintf("%s.pcm", event.(openairt.ResponseAudioDoneEvent).EventID), fulldata, 0o644)
+			streamer := pcm.NewPCMStream(
+				fulldata,
+				beep.Format{
+					SampleRate:  beep.SampleRate(24000),
+					NumChannels: 1,
+					Precision:   2, // 16-bit PCM
+				},
+			)
+			datas = make(chan []byte, 100)
+			streamers <- streamer
+		}
+	}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case streamer := <-streamers:
+				done := make(chan bool, 1)
+				speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+					done <- true
+				})))
+				<-done // wait for done
+			}
+		}
+	}()
+
+	connHandler := openairt.NewConnHandler(ctx, conn, logHandler, responseHandler, responseDeltaHandler, audioResponseHandler)
+	connHandler.Start()
+
+	err = conn.SendMessage(ctx, openairt.SessionUpdateEvent{
+		Session: openairt.ClientSession{
+			Modalities:        []openairt.Modality{openairt.ModalityText, openairt.ModalityAudio},
+			Voice:             openairt.VoiceShimmer,
+			OutputAudioFormat: openairt.AudioFormatPcm16,
+			InputAudioTranscription: &openairt.InputAudioTranscription{
+				Model: openai.Whisper1,
+			},
+			TurnDetection:   nil,
+			MaxOutputTokens: 4000,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("Conversation (input 'start' to start recording)")
+	fmt.Println("---------------------")
+	fmt.Print("> ")
+	s := bufio.NewScanner(os.Stdin)
+
+	// Audio init
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	var data io.Reader
+	var stop func()
+
+Loop:
+	for s.Scan() {
+
+		switch s.Text() {
+		case "exit", "quit":
+			break Loop
+		case "start":
+			data, stop, err = recorder.Record(1, 24000, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Recording...(input 'stop' to stop recording)")
+			fmt.Print("> ")
+		case "stop":
+			if stop == nil {
+				log.Print("not recording")
+				continue
+			}
+			stop()
+			audioData, err := io.ReadAll(data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			base64Audio := base64.StdEncoding.EncodeToString(audioData)
+			conn.SendMessage(ctx, openairt.InputAudioBufferAppendEvent{
+				Audio: base64Audio,
+			})
+			conn.SendMessage(ctx, openairt.InputAudioBufferCommitEvent{})
+			conn.SendMessage(ctx, openairt.ResponseCreateEvent{
+				Response: openairt.ResponseCreateParams{
+					Modalities: []openairt.Modality{openairt.ModalityText, openairt.ModalityAudio},
+				},
+			})
+		}
+
+	}
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,26 @@ module aivoice
 go 1.24.4
 
 require (
+	github.com/WqyJh/go-openai-realtime v0.6.0
+	github.com/WqyJh/go-openai-realtime/examples v0.0.0-20250615101212-83067fc75fd6
+	github.com/faiface/beep v1.1.0
+	github.com/gordonklaus/portaudio v0.0.0-20250206071425-98a94950218b
 	github.com/gorilla/websocket v1.5.3
 	github.com/joho/godotenv v1.5.1
 	github.com/openai/openai-go/v2 v2.3.0
+	github.com/sashabaranov/go-openai v1.41.1
 )
 
 require (
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 )
